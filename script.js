d527fb799655840d3a3e195547319a081d4b40d9
SnusPower <121939990+azimkaxramonov@users.noreply.github.com>
2023-01-28 12:03:06 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+ alert('Welcome To Burger King')
+
+ let menu = 'cheeseburger wopper doubleburger '
+ 
+ let choice = prompt(' our menu cheeseburger wopper doubleburger ' ).trim()
+ 
+ let drink = (' pepsi mirinda lipton ')
+
+ let dessert =(' icecream cake snikers ')
+  
+ if(menu .includes(choice)){
+    console.log('ok')
+    alert('ok')
+    let not = confirm(' Do you want to order anythung else from drinks')
+    if(not === true){
+
+    let choicedrink = prompt(' our drink menu pepsi mirinda lipton').trim()
+    if(drink .includes(choicedrink)){
+        console.log('ok')
+        alert('ok')
+       let nots =confirm(' Do you want to order anythung else from dessert ')
+       
+       let dessertchoice = prompt(' our dessert menu icecream cake snikers ')
+       if(nots === true)
+       {
+     
+        if(dessert .includes(dessertchoice)){
+console.log('ok')
+alert('your order?')
+confirm( choice + ' ' +  choicedrink  + ' ' +   dessertchoice)
+        }
+
+
+    } else{ alert(' not on the menu ')}
+ 
+    }else{alert('its a pity')}
+
+
+    }else{alert('its a pity')}
+
+
+}else{ alert(' not on the menu ')}
+ 
